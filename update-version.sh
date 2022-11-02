2e939bb9f93635eb121810ffd4336304a92ab80b
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-11-02 15:41:19 -0600

    Initial commit

---
+++ update-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+NEW_VERSION="20211004184552"
+
+if [ -z "$1" ]; then
+  echo "New version not specified as first arg.  Using ${NEW_VERSION}.  Make sure that's what you want"
+else
+  echo "Setting new version to '${1}'"
+  NEW_VERSION="$1"
+fi
+
+K8S_FILES=("k8s/staging/deploy.yaml" "k8s/prod/deploy.yaml")
+
+for f in ${K8S_FILES[@]}; do
+  sed -i -E -e "s|image: docker.io/freedomben/domain-namme-operator(.*):.*|image: docker.io/freedomben/domain-namme-operator:${NEW_VERSION}|g" "$f"
+done
